2c6d15b706c4bf2c2db7a8270c69b1d9880dc26d
Daniel Oppenhuizen <111583495+Skyerial@users.noreply.github.com>
2024-08-01 09:46:34 +0200

    fix highlight planet

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -1,30 +1,34 @@
 extends Node3D
 
-@onready var shader: ShaderMaterial = $CSGSphere3D.material.next_pass
+@export var highlight_shader: ShaderMaterial
 
 signal planet_selected
 
+@onready var planet_material: StandardMaterial3D = $CSGSphere3D.material
+var currently_selected: bool = false
 var placed: bool = true
 
 
 func _process(delta: float) -> void:
-    if !placed:
-        var camera = get_viewport().get_camera_3d()  # Get the current camera
-        var mouse_position = get_viewport().get_mouse_position()
+	if !placed:
+		var camera = get_viewport().get_camera_3d()  # Get the current camera
+		var mouse_position = get_viewport().get_mouse_position()
 
-        # Create a ray from the camera through the mouse position
-        var ray_origin = camera.project_ray_origin(mouse_position)
-        var ray_direction = camera.project_position(mouse_position, ray_origin.z)
-        position = ray_direction
+		# Create a ray from the camera through the mouse position
+		var ray_origin = camera.project_ray_origin(mouse_position)
+		var ray_direction = camera.project_position(mouse_position, ray_origin.z)
+		position = ray_direction
 
 func _on_area_3d_mouse_entered() -> void:
-    shader.set_shader_parameter("strength", 0.5)
+	planet_material.next_pass = highlight_shader
 
 func _on_area_3d_mouse_exited() -> void:
-    shader.set_shader_parameter("strength", 0)
+	if !currently_selected:
+		planet_material.next_pass = null
 
 func _on_area_3d_input_event(camera: Node, event: InputEvent, position: Vector3,
-                            normal: Vector3, shape_idx: int) -> void:
-    if event is InputEventMouseButton and placed:
-        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-            planet_selected.emit(self)
+							normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and placed:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			planet_selected.emit(self)
+			currently_selected = true
--- scripts/planet_controller.gd
+++ scripts/planet_controller.gd
@@ -8,30 +8,33 @@ var preview_planet: bool = false
 
 
 func _ready() -> void:
-    planet_sun = get_child(0)
-    planet_sun.planet_selected.connect(set_selected_planet)
+	planet_sun = get_child(0)
+	planet_sun.planet_selected.connect(set_selected_planet)
 
 func _process(delta: float) -> void:
-    check_and_act_input()
+	check_and_act_input()
 
 func check_and_act_input() -> void:
-    if Input.is_action_just_pressed("duplicate") and selected_planet != null and !preview_planet:
-        print("duplicating")
-        # somehow duplicate and attach planet to cursor
-        preview_planet = true
-        var planet_instance: Node3D = planet_scene.instantiate()
-        planet_instance.planet_selected.connect(set_selected_planet)
-        planet_instance.placed = false
-        set_selected_planet(planet_instance)
-        add_child(planet_instance)
+	if Input.is_action_just_pressed("duplicate") and selected_planet != null and !preview_planet:
+		preview_planet = true
+		var planet_instance: Node3D = planet_scene.instantiate()
+		planet_instance.planet_selected.connect(set_selected_planet)
+		planet_instance.placed = false
+		set_selected_planet(planet_instance)
+		add_child(planet_instance)
 
-    if Input.is_action_pressed("select") and preview_planet:
-        # set the planet to stop following the cursor
-        selected_planet.placed = true
-        preview_planet = false
+	elif Input.is_action_pressed("select") and preview_planet:
+		# set the planet to stop following the cursor
+		selected_planet.placed = true
+		selected_planet = null
+		preview_planet = false
+		
+	elif Input.is_action_pressed("select") and selected_planet != null:
+		selected_planet.currently_selected = false
+		selected_planet = null
 
 func set_selected_planet(planet: Node3D) -> void:
-    if planet != selected_planet:
-        selected_planet = planet
-    else:
-        selected_planet = null
+	if planet != selected_planet:
+		selected_planet = planet
+	else:
+		selected_planet = null
